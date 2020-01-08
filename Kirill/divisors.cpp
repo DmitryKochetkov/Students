bdd929e28008d78eb7ad407ab1268da46c724348
DmitryKochetkov <34890452+DmitryKochetkov@users.noreply.github.com>
2020-01-08 12:56:45 +0300

    Nikita: StringLab

--- Kirill/divisors.cpp
+++ Kirill/divisors.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
@@ -17,6 +18,18 @@ int main() {
 }
 
 void get_odd_even_dividors(int n, int &odd, int& even) {
+    clock_t start_time = clock();
+    for (int i = 1; i <= n; i += 2) {
+        if (n % i == 0) {odd++;}
+    }
+
+    for (int i = 2; i <= n; i += 2) {
+        if (n % i == 0) {even++;}
+    }
+    clock_t end_time = clock();
+    cout << "Time 1: " << (double)(end_time - start_time) / (double)CLOCKS_PER_SEC << endl;
+
+    start_time = clock();
     for (int i = 1; i <= n/2; i += 2) {
         if (n % i == 0) {odd++;}
     }
@@ -24,6 +37,8 @@ void get_odd_even_dividors(int n, int &odd, int& even) {
     for (int i = 2; i <= n/2; i += 2) {
         if (n % i == 0) {even++;}
     }
+    end_time = clock();
+    cout << "Time 2: " << (double)(end_time - start_time) / (double)CLOCKS_PER_SEC << endl;
 
     if (n % 2 == 0)
         even++;
---
+++ Nikita/StringLab/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Лабораторная работа из универа: написать функцию, переводящую слова строки в нижний регистр.
+ * Если слово не изменено, удалить его из результирующей строки.
+ */
+
+namespace StringLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string converter(string s)
+        {
+            string res = "";
+
+            while (s.Contains(' '))
+            {
+                string word = s.Substring(0, s.IndexOf(' ') + 1);
+                if (word != word.ToLower()) res += word.ToLower();
+                s = s.Remove(0, s.IndexOf(' ') + 1);
+            }
+
+            if (s != s.ToLower()) res += s.ToLower();
+
+            return res;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = converter(textBox1.Text);
+        }
+    }
+}
